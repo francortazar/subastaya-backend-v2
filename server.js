2e568f87375586486863dcbf2e7832de422dc41e
Francortazar <franciscocortazar02@gmail.com>
2026-04-27 16:02:40 -0300

    fix puerto render definitivo

--- server.js
+++ server.js
@@ -1465,4 +1465,8 @@ if (ocultasIds.includes(String(s.id))) continue;
   }
 });
 
-app.listen(5000, () => console.log("Servidor corriendo en http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => 
+  console.log(`Servidor corriendo en puerto ${PORT}`)
+);
